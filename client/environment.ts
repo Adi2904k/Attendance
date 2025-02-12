249b7cb6d8f205f6d1bfbb8cfe4bff235176fda5
Adi2904k <adityaamalusare14@gmail.com>
2025-02-12 17:00:50 +0530

    env.ts ignore

--- client/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    // production: true,
-    // backendBaseUrl: 'https://attendance-pi-ruby.vercel.app'
-    production: false,
-    backendBaseUrl: 'http://localhost:3000'
-
-  };
